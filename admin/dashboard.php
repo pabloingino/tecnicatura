f3f33f898368effe71eb61545f12be1e9bb90843
pingino <pingino@mininterior.gob.ar>
2018-10-23 08:30:27 -0300

    se arreglo el dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -23,11 +23,11 @@
 
     <!-- Main content -->
     <section class="content">
-        <div class="row">
+        <!-- <div class="row">
             <div class="box-body chart-responsive">
               <div class="chart" id="grafica-registros" style="height: 300px;"></div>
             </div>
-        </div>
+        </div> -->
 
 
         <h2 class="page-header">Resumen de Registros</h2>
